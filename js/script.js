cc0b749715efabdccd456f72c4820eda207e721c
Liya-Getachew <113055994+Liya-Getachew@users.noreply.github.com>
2022-10-15 23:56:59 +0000

    added js

--- js/script.js
+++ js/script.js
@@ -4,7 +4,17 @@
 // Created on: Oct 15 2022
 // This file contains the JS functions for index.html
 
-"use strict"
+'use strict'
 /**
  * This function calculates the perimeter of a square.
  */
+function calculateClicked() {
+  // input
+  const side = parseInt(document.getElementById('side-length').value)
+
+  // process
+  const perimeter = side * 4
+
+  // output
+  document.getElementById('perimeter').innerHTML = '<p>Perimeter = ' + perimeter + ' cm.'
+}
