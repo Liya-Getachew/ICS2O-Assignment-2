e2cb71a50667e7e6c2f9abe0aabd2442185844c3
Liya-Getachew <Liya-Getachew@users.noreply.github.com>
2022-10-15 20:32:12 +0000

    Prettified Code!

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@
 // Created on: Oct 15 2022
 // This file contains the JS functions for index.html
 
-'use strict'
+"use strict"
 /**
-  * This function calculates the perimeter of a square.
-  */
+ * This function calculates the perimeter of a square.
+ */
